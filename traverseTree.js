10020c8ec9fe1535abe9b2564702c1a3e2cb3d27
shinobig <shinobigakiya@gmail.com>
2022-03-08 19:50:22 -0600

    meta

--- traverseTree.js
+++ traverseTree.js
@@ -91,23 +91,111 @@ tree.insert(8);
 tree.insert(20);
 
 
+// function breadthFrist(tree) {
+//   let queue = [];
+//   let visited = [];
+//   let depth = 1
+//   queue.push(tree.root);
+//
+//   while (queue.length > 0) {
+//     let node = queue.shift();
+//     if (node.left) queue.push(node.left);
+//     if (node.right) queue.push(node.right);
+//     visited.push(node);
+//   }
+//
+//   return visited.map(node => node.val);
+// }
+
+
 function breadthFrist(tree) {
-  let queue = [];
+  let queue = [[tree.root]];
   let visited = [];
-  queue.push(tree.root);
+
+  let currentQueue = [];
+
+
   while (queue.length > 0) {
-    let node = queue.shift();
-    if (node.left) {
-      queue.push(node.left);
+
+    currentQueue = queue.shift();
+    let visitedArr = [];
+    let newQueueToAdd = [];
+
+
+    while (currentQueue.length > 0) {
+      let node = currentQueue.shift();
+      if (node.left) newQueueToAdd.push(node.left);
+      if (node.right) newQueueToAdd.push(node.right);
+      visitedArr.push(node.val);
     }
-    if (node.right) {
-      queue.push(node.right);
+
+    if(visitedArr.length > 0){
+
+    visited.push(visitedArr);
     }
-    visited.push(node);
+    if(newQueueToAdd.length > 0){
+
+    queue.push(newQueueToAdd);
+    }
   }
 
-  let result = visited.map(node => node.val);
-  return result;
+return visited
 }
 
 console.log(breadthFrist(tree));
+
+
+//  DEPTH FIRST
+
+function preOrder(tree) {
+// return the values in order to rebuild the tree
+  let visited = [];
+
+  function traverse(node) {
+    visited.push(node);
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+  }
+
+  traverse(tree.root);
+
+  return visited.map(node => node.val);
+}
+
+// console.log(preOrder(tree));
+
+function postOrder(tree) {
+  let visited = [];
+
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+    visited.push(node);
+  }
+
+  traverse(tree.root);
+
+  return visited.map(node => node.val);
+
+}
+
+// console.log(postOrder(tree));
+
+function inOrder(tree) {
+
+  // Return the tree in order from smallest to biggest
+  let visited = [];
+
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    visited.push(node);
+    if (node.right) traverse(node.right);
+  }
+
+  traverse(tree.root);
+  return visited.map(node => node.val);
+}
+
+// console.log(inOrder(tree));
+
+// console.log(Math.ceil(Math.sqrt(16)));
